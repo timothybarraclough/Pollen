585bace0f700431ce9e2a2c9896b87b072df1144
Kieran Trezona-le Comte <kieran@fluxfederation.com>
2017-10-19 11:46:02 +1300

    Add dev DB setup instructions to README, use pollen user & pollen_dev db

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # Pollen
+
+## Dev DB Setup
+
+```
+$ createdb pollen_dev
+
+$ psql -U postgres
+> CREATE ROLE pollen;
+> ALTER ROLE pollen with LOGIN;
+> ALTER ROLE pollen with SUPERUSER;
+```
--- src/Config.hs
+++ src/Config.hs
@@ -106,4 +106,4 @@ envPool Production = 8
 -- | A basic 'ConnectionString' for local/test development. Pass in either
 -- @""@ for 'Development' or @"test"@ for 'Test'.
 connStr :: BS.ByteString -> ConnectionString
-connStr sfx = "host=localhost dbname=timothybarraclough" <> sfx <> " user=timothybarraclough password= port=5432"
+connStr sfx = "host=localhost dbname=pollen_dev" <> sfx <> " user=pollen password= port=5432"
